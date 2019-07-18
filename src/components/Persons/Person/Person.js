b4f5e5c42e38932e113a97f785cf76da559eeb54
ALEX Solutions <alexsolutions@annette-ong.local>
2019-07-19 08:46:14 +1000

    Render adj JSX using inbuilt hoc React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,21 +1,19 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import classes from './Person.module.css';
-import Aux from '../../../hoc/Aux';
 
 // const person = (props) => { & no render()
 class Person extends Component {
     render () {
         console.log('[Person.js] rendering...');
 
-        return (
-            // <Aux> is ONE expression, so this works.
-            <Aux>
+        return(
+            <Fragment>
                 <p key="i1" onClick={this.props.click}>
                     I'm {this.props.name} and I am {this.props.age} years old!
                     </p>
                 <p key="i2">{this.props.children}</p>
                 <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />
-            </Aux>
+            </Fragment>
         );
     }
 }
