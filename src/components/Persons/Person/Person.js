3bc4ad7374fa31a1cf0f88f449d163f573303a30
ALEX Solutions <alexsolutions@annette-ong.local>
2019-07-19 10:31:10 +1000

    Reference to specially refer to 1 particular elem

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,6 +7,28 @@ import withClass from '../../../hoc/withClass';
 
 // const person = (props) => { & no render()
 class Person extends Component {
+    // method 2:
+    constructor(props) {
+        super(props); // always call super when making constructor
+        this.inputElementRef = React.createRef();
+    }
+
+
+    // only selects first component of 'input' type in entire page
+    // not great because it doesn't refer to input rendered by Person
+    // if it did, it would render the last input
+    // this is because it's not related to React, and so doesn't care about React,
+    // but is a general web/browser feature i.e. DOM selector
+    // and always works on entire DOM
+    
+    componentDidMount() {
+    // document.querySelector('input').focus();
+        // method 1:
+        // this.inputElement.focus();
+        // method 2:
+        this.inputElementRef.current.focus();
+    }
+
     render () {
         console.log('[Person.js] rendering...');
 
@@ -16,7 +38,16 @@ class Person extends Component {
                     I'm {this.props.name} and I am {this.props.age} years old!
                     </p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                <input 
+                    /* this.inputElement is created as a global property
+                    that can be used anywhere from now on */
+                    // method 1:
+                    // ref={(inputEl) => {this.inputElement = inputEl}}
+                    // method 2:
+                    ref={this.inputElementRef}
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name} />
             </Aux>
         );
     }
